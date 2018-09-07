f910473657206088d4e7320a7591cf3aa6b85538
soc-macmini-15 <soc-macmini-15@soc-admins-Mac-mini-72.local>
2018-09-07 17:22:02 +0530

    MediaPlayer Added

--- app/src/main/java/com/example/soc_macmini_15/musicplayer/Activity/MainActivity.java
+++ app/src/main/java/com/example/soc_macmini_15/musicplayer/Activity/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -19,36 +21,35 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.TableLayout;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import com.example.soc_macmini_15.musicplayer.Adapter.ViewPagerAdapter;
-import com.example.soc_macmini_15.musicplayer.Helper.SongsManager;
+import com.example.soc_macmini_15.musicplayer.Fragments.TabFragment;
 import com.example.soc_macmini_15.musicplayer.R;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, TabFragment.createDataParse {
 
     private ImageButton imgBtnPlayPause;
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private SeekBar seekbarController;
 
     private DrawerLayout mDrawerLayout;
     private boolean countPlayPause = true;
     private final int MY_PERMISSION_REQUEST = 100;
 
+    MediaPlayer mediaPlayer;
+    Handler handler;
+    Runnable runnable;
 
 
     @Override
@@ -58,6 +59,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         init();
         grantedPermission();
 
+    }
+
+    private void init() {
+        seekbarController = findViewById(R.id.seekbar_controller);
+        viewPager = findViewById(R.id.songs_viewpager);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        imgBtnPlayPause = findViewById(R.id.img_btn_play);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        handler = new Handler();
+        mediaPlayer = new MediaPlayer();
+
+        toolbar.setTitleTextColor(getResources().getColor(R.color.text_color));
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.menu_icon);
+
+        imgBtnPlayPause.setOnClickListener(this);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                switch (item.getItemId()) {
+                    case R.id.nav_about:
+                        about();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     private void grantedPermission() {
@@ -74,30 +109,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 snackbar.show();
             }
         } else {
-              fetchSongs();
+            fetchSongs();
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
+        switch (requestCode) {
             case MY_PERMISSION_REQUEST:
-                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                        if(ContextCompat.checkSelfPermission(MainActivity.this,
-                                Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
-                            Toast.makeText(this,"Permission Granted!",Toast.LENGTH_SHORT).show();
-                                fetchSongs();
-                        }
-                        else{
-                            Snackbar snackbar = Snackbar.make(mDrawerLayout, "Provide the StoragePermission", Snackbar.LENGTH_LONG);
-                            snackbar.show();
-                            finish();
-                        }
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "Permission Granted!", Toast.LENGTH_SHORT).show();
+                        fetchSongs();
+                    } else {
+                        Snackbar snackbar = Snackbar.make(mDrawerLayout, "Provide the StoragePermission", Snackbar.LENGTH_LONG);
+                        snackbar.show();
+                        finish();
+                    }
                 }
         }
     }
-    private void fetchSongs(){
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(),getContentResolver());
+
+    private void fetchSongs() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), getContentResolver());
         viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
@@ -121,37 +156,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-    }
-
-    private void init() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitleTextColor(getResources().getColor(R.color.text_color));
-        setSupportActionBar(toolbar);
-
-        ActionBar actionBar = getSupportActionBar();
-        assert actionBar != null;
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeAsUpIndicator(R.drawable.menu_icon);
-
-        mDrawerLayout = findViewById(R.id.drawer_layout);
-        imgBtnPlayPause = findViewById(R.id.img_btn_play);
-        imgBtnPlayPause.setOnClickListener(this);
-
-        viewPager = findViewById(R.id.songs_viewpager);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                item.setChecked(true);
-                mDrawerLayout.closeDrawers();
-                switch (item.getItemId()) {
-                    case R.id.nav_about:
-                        about();
-                        break;
-                }
-                return true;
-            }
-        });
     }
 
     private void about() {
@@ -199,4 +203,77 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private void attachMusic(String name, String path) {
+        mediaPlayer.reset();
+        try {
+            mediaPlayer.setDataSource(path);
+            mediaPlayer.prepare();
+            setControls();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setControls() {
+
+        seekbarController.setMax(mediaPlayer.getDuration());
+        playCycle();
+        mediaPlayer.start();
+
+        seekbarController.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void playCycle() {
+        seekbarController.setProgress(mediaPlayer.getCurrentPosition());
+        if (mediaPlayer.isPlaying()) {
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    playCycle();
+
+                }
+            };
+            handler.postDelayed(runnable, 100);
+        }
+    }
+
+    @Override
+    public void onDataPass(String name, String path) {
+        Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+        attachMusic(name, path);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.release();
+        handler.removeCallbacks(runnable);
+    }
 }
--- app/src/main/java/com/example/soc_macmini_15/musicplayer/Fragments/TabFragment.java
+++ app/src/main/java/com/example/soc_macmini_15/musicplayer/Fragments/TabFragment.java
@@ -2,6 +2,7 @@ package com.example.soc_macmini_15.musicplayer.Fragments;
 
 
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -30,10 +31,14 @@ public class TabFragment extends ListFragment {
 
     private static ContentResolver contentResolver1;
     public ArrayList<String> songsList;
+    public ArrayList<String> pathList;
+    public ArrayList<String> nameList;
+
+
     private ListView listView;
     private ArrayAdapter<String> adapter;
 
-
+    private createDataParse createDataParse;
     private int position;
     private TextView textView;
     private ContentResolver contentResolver;
@@ -43,7 +48,7 @@ public class TabFragment extends ListFragment {
         bundle.putInt("pos", position);
         TabFragment tabFragment = new TabFragment();
         tabFragment.setArguments(bundle);
-        contentResolver1=mcontentResolver;
+        contentResolver1 = mcontentResolver;
         return tabFragment;
     }
 
@@ -53,6 +58,11 @@ public class TabFragment extends ListFragment {
 
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        createDataParse = (createDataParse) context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -61,16 +71,16 @@ public class TabFragment extends ListFragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-
         listView = view.findViewById(R.id.list_playlist);
         position = getArguments().getInt("pos");
-        contentResolver=contentResolver1;
+        contentResolver = contentResolver1;
         setContent();
     }
 
     public void setContent() {
         songsList = new ArrayList<>();
+        pathList = new ArrayList<>();
+        nameList = new ArrayList<>();
         getMusic();
         adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, songsList);
         listView.setAdapter(adapter);
@@ -78,7 +88,10 @@ public class TabFragment extends ListFragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getContext(), "You clicked :\n"+songsList.get(position), Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getContext(), "You clicked :\n" + songsList.get(position), Toast.LENGTH_SHORT).show();
+
+                createDataParse.onDataPass(nameList.get(position), pathList.get(position));
+
             }
         });
     }
@@ -90,15 +103,21 @@ public class TabFragment extends ListFragment {
         if (songCursor != null && songCursor.moveToFirst()) {
             int songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
             int songArtist = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
-            int songPath=songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
+            int songPath = songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
 
             do {
                 String currentTitle = songCursor.getString(songTitle);
                 String currentArtist = songCursor.getString(songArtist);
-                String currentLocation=songCursor.getString(songPath);
-                songsList.add(currentTitle + "\n" + currentArtist + "\n"+currentLocation);
+                String currentLocation = songCursor.getString(songPath);
+                pathList.add(currentLocation);
+                nameList.add(currentTitle);
+                songsList.add(currentTitle + "\n" + currentArtist);
             } while (songCursor.moveToNext());
         }
+    }
+
+    public interface createDataParse {
+        public void onDataPass(String name, String path);
     }
 
 }
